09c15a2ed08862dad1fcc17707f246c6b85ae19c
NiyathaVS <niyatha2001@gmail.com>
2026-08-24 12:48:58 +0530

    Added frontend files

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: process.env.VITE_PORT || 5173,
+        host: process.env.VITE_HOST || "localhost",
+        proxy: {
+            "/traces": {
+                target: process.env.VITE_API_URL || "http://localhost:8080",
+                changeOrigin: true
+            }
+        }
+    }
+});
